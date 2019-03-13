d011d1e58ff5b643763532b7032a07ba3747dd1b
enzoftware <lizama.enzo@gmail.com>
2019-03-13 17:29:48 -0500

    Tabs for home page

--- lib/screens/home/widgets/tabs.dart
+++ lib/screens/home/widgets/tabs.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 import '../model/tab_item.dart';
+import '../tabs/dashboard.dart' as _dashboardTab;
+import '../tabs/goals.dart' as _goalsTab;
+import '../tabs/ranking.dart' as _rankingTab;
 
 class Tabs extends StatefulWidget {
   @override
@@ -14,7 +19,45 @@ class _TabsState extends State<Tabs> {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      // APP BAR
+      appBar: AppBar(
+        title: Text(
+          _titleApp,
+          style: TextStyle(
+            fontSize: Theme
+                .of(context)
+                .platform == TargetPlatform.iOS ? 17 : 20,
+          ),
+        ),
+        elevation: Theme
+            .of(context)
+            .platform == TargetPlatform.iOS ? 0 : 4,
+      ),
+      // CONTENT OF TABS
+      body: PageView(
+        controller: _tabController,
+        onPageChanged: onTabChanged,
+        children: <Widget>[
+          _dashboardTab.Dashboard(),
+          _rankingTab.Ranking(),
+          _goalsTab.Goals()
+        ],
+      ),
+      // TABS
 
+      bottomNavigationBar: Theme
+          .of(context)
+          .platform == TargetPlatform.iOS ?
+      // FOR iOS PLATFORM
+      CupertinoTabBar(
+          items: null
+      ) :
+      // FOR Android PLATFORM
+      BottomNavigationBar(
+          items: null
+      ),
+    );
   }
 
   @override
@@ -31,4 +74,8 @@ class _TabsState extends State<Tabs> {
     _tabController.dispose();
   }
 
+
+  void onTabChanged(int value) {
+
+  }
 }
