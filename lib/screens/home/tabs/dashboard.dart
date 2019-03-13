67a1242a19f9abbc0a073b4a639d2341b9bf99f6
enzoftware <lizama.enzo@gmail.com>
2019-03-12 19:57:13 -0500

    Building the base tabs items

--- lib/screens/home/tabs/dashboard.dart
+++ lib/screens/home/tabs/dashboard.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Icon(Icons.assessment, size: 150, color: Colors.black54,),
+          Text("Dashboard")
+        ],
+      ),
+    );
   }
 }
--- lib/screens/home/tabs/goals.dart
+++ lib/screens/home/tabs/goals.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 class Goals extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Icon(Icons.assistant_photo, size: 150, color: Colors.black54,),
+          Text("Dashboard")
+        ],
+      ),
+    );
   }
 }
