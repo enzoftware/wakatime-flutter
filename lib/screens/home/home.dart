c3cc6721a18cec91ecb7f81d44b7dcf0175e41be
enzoftware <lizama.enzo@gmail.com>
2019-03-17 13:30:56 -0500

    Set MaterialApp and set main colors

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,12 +1,19 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 import 'widgets/tabs.dart';
 
 class WakatimeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      title: 'Flutter Starter',
+      theme: new ThemeData(
+          primarySwatch: Colors.blueGrey,
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: Colors.blueGrey, backgroundColor: Colors.white
+      ),
+      home: new Tabs(),
     );
   }
 }
