8ef829e6b677e03a6d37bb3dba65c5352477cb60
enzoftware <lizama.enzo@gmail.com>
2019-03-17 18:53:37 -0500

    Handling future get user information to decide which screen open

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:wakatime_client/data/api/wakatime_api.dart';
 import 'package:wakatime_client/data/model/user.dart';
 import 'package:wakatime_client/screens/login/login_screen.dart';
 
+import 'data/api/wakatime_api.dart';
 import 'screens/home/home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  bool isApiKeyCorrect = false;
+
   @override
   Widget build(BuildContext context) {
-    final _u =  WakaTimeApi().getUserInformation();
-    _u.then((user){
-      final User u = user;
-      print(u.email);
-    });
-    return _u == null ? WakatimeApp() : LoginScreen();
+    final Future<User> _u = WakaTimeApi().getUserInformation();
+    _u.then(onValue)
+        .catchError(onError);
+    sleep(new Duration(seconds: 5));
+    return isApiKeyCorrect ? WakatimeApp() : LoginScreen();
+  }
+
+  void onError() {
+    isApiKeyCorrect = false;
+  }
+
+  void onValue(User value) {
+    print(value.email);
+    isApiKeyCorrect = true;
   }
 }
